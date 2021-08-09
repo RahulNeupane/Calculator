b5b8270c3a07a3db099622823fee5015fbe4b427
Rahul Neupane <nrahul503@gmail.com>
2021-08-09 13:52:11 +0545

    Initial commit

---
+++ Calculator/Form1.cs
@@ -0,0 +1,189 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculator
+{
+    public partial class Calculator : Form
+    {
+        public Calculator()
+        {
+            InitializeComponent();
+        }
+
+        string operation = "";
+        decimal old_value = 0;
+
+        private void setvalue(string data)
+        {
+            if (maintext.Text == "+" || maintext.Text == "-" || maintext.Text == "*" || maintext.Text == "/")
+            {
+                maintext.Text = "";
+            }
+            string number = data;
+            maintext.Text = maintext.Text + number;
+
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            setvalue("6");
+        }
+
+        private void button12_Click(object sender, EventArgs e)
+        {
+            setvalue("1");
+
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            setvalue("2");
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            setvalue("3");
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            setvalue("4");
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            setvalue("5");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            setvalue("7");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            setvalue("8");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            setvalue("9");
+        }
+
+        private void button16_Click(object sender, EventArgs e)
+        {
+            setvalue("0");
+        }
+
+        private void button14_Click(object sender, EventArgs e)
+        {
+            string number = ".";
+            maintext.Text = maintext.Text + number;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            string current_val = maintext.Text;
+            old_value = Convert.ToDecimal(current_val);
+
+            string oper = "+";
+            maintext.Text = oper;
+            operation = oper;
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            string current_val = maintext.Text;
+            old_value = Convert.ToDecimal(current_val);
+
+            string oper = "-";
+            maintext.Text = oper;
+            operation = oper;
+
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            string current_val = maintext.Text;
+            old_value = Convert.ToDecimal(current_val);
+
+            string oper = "*";
+            maintext.Text = oper;
+            operation = oper;
+
+        }
+
+        private void button13_Click(object sender, EventArgs e)
+        {
+            string current_val = maintext.Text;
+            old_value = Convert.ToDecimal(current_val);
+
+            string oper = "/";
+            maintext.Text = oper;
+            operation = oper;
+
+        }
+
+        private void button15_Click(object sender, EventArgs e)
+        {
+            string new_val = maintext.Text;
+            decimal new_value = Convert.ToDecimal(new_val);
+
+            switch(operation)
+            {
+                case "+":
+                    decimal added_value = old_value + new_value;
+                    old_value = added_value;
+                    maintext.Text = added_value.ToString();
+                    break;
+                case "-":
+                    decimal subbed_value = old_value - new_value;
+                    old_value = subbed_value;
+                    maintext.Text = subbed_value.ToString();
+                    break;
+                case "*":
+                    decimal multiplied_value = old_value * new_value;
+                    old_value = multiplied_value;
+                    maintext.Text = multiplied_value.ToString();
+                    break;
+                case "/":
+                    if (new_value > 0 || new_value < 0)
+                    {
+                        decimal divided_value = old_value / new_value;
+                        old_value = divided_value;
+                        maintext.Text = divided_value.ToString();
+
+                    }
+
+                    else
+                        MessageBox.Show("Divide by zero error");
+                    break;
+            }
+        }
+
+        private void button17_Click(object sender, EventArgs e)
+        {
+            old_value = 0;
+            maintext.Text = "";
+            operation = "";
+        }
+
+        
+
+        private void button18_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            New_calculator calc = new New_calculator();
+            calc.ShowDialog();
+            this.Show();
+        }
+    }
+}
---
+++ Calculator/New calculator.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculator
+{
+    public partial class New_calculator : Form
+    {
+        public New_calculator()
+        {
+            InitializeComponent();
+        }
+
+        string operation = "";
+        decimal old = 0;
+
+        private void setvalue (Button data)
+        {
+            if (maintext.Text == "+" || maintext.Text == "*" || maintext.Text == "-" || maintext.Text == "/")
+            {
+                maintext.Text = "";
+
+            }
+            Button button = data;
+            maintext.Text = maintext.Text + button.Text;
+        }
+
+        private void button_click(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+            setvalue(button);
+        }
+
+        private void operator_click(object sender, EventArgs e)
+        {
+
+            string cur = maintext.Text;
+            old = Convert.ToDecimal(cur);
+
+            string oper = "+";
+            maintext.Text = oper;
+            operation = oper;
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            string cur = maintext.Text;
+            old = Convert.ToDecimal(cur);
+
+            string oper = "-";
+            maintext.Text = oper;
+            operation = oper;
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            string cur = maintext.Text;
+            old = Convert.ToDecimal(cur);
+
+            string oper = "*";
+            maintext.Text = oper;
+            operation = oper;
+        }
+
+        private void button13_Click(object sender, EventArgs e)
+        {
+            string cur = maintext.Text;
+            old = Convert.ToDecimal(cur);
+
+            string oper = "/";
+            maintext.Text = oper;
+            operation = oper;
+        }
+
+        private void button15_Click(object sender, EventArgs e)
+        {
+            string recent = maintext.Text;
+            decimal recent_val = Convert.ToDecimal(recent);
+
+            switch(operation)
+            {
+                case "+":
+                    decimal added = old + recent_val;
+                    old = added;
+                    maintext.Text = added.ToString();
+                    break;
+                case "-":
+                    decimal subbed = old - recent_val;
+                    old = subbed;
+                    maintext.Text = subbed.ToString();
+                    break;
+                case "*":
+                    decimal product = old * recent_val;
+                    old = product;
+                    maintext.Text = product.ToString();
+                    break;
+                case "/":
+                    if(recent_val == 0)
+                    {
+                        MessageBox.Show("CAN'T DIVIDE BY ZERO(0)");
+                    }
+                    else
+                    {
+                        decimal divided = old / recent_val;
+                        old = divided;
+                        maintext.Text = divided.ToString();
+                       
+
+                    }
+                    break;
+
+            }
+            
+        }
+
+        private void button17_Click(object sender, EventArgs e)
+        {
+            old = 0;
+            maintext.Text = "";
+            operation = "";
+        }
+    }
+}
